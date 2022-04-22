8aa3d0f7f8c3d39da24a10c3307ec5a4115b0870
GayatriMhetre21 <mhetre.gayatri1510@gmail.com>
2022-04-22 21:39:20 +0530

    begin

--- app/src/main/java/com/example/tanayanotes/model/Adapter.java
+++ app/src/main/java/com/example/tanayanotes/model/Adapter.java
@@ -1,5 +1,6 @@
 package com.example.tanayanotes.model;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,11 +8,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tanayanotes.R;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     List<String> titles;
@@ -36,13 +40,31 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.noteTitle.setText(titles.get(position));
         holder.noteContent.setText(content.get(position));
+        holder.mCardView.setCardBackgroundColor(holder.view.getResources().getColor(getRandomColor(),null));
 
-        holder.view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(), "The Item is Clicked.", Toast.LENGTH_SHORT).show();
-            }
+        holder.view.setOnClickListener((v) -> {
+            Toast.makeText(v.getContext(), "The item is clicked", Toast.LENGTH_SHORT).show();
+
         });
+    }
+
+    private int getRandomColor() {
+
+        List<Integer>colorCode = new ArrayList<>();
+        colorCode.add(R.color.blue);
+        colorCode.add(R.color.yellow);
+        colorCode.add(R.color.skyblue);
+        colorCode.add(R.color.lightPurple);
+        colorCode.add(R.color.lightGreen);
+        colorCode.add(R.color.gray);
+        colorCode.add(R.color.pink);
+        colorCode.add(R.color.red);
+        colorCode.add(R.color.greenlight);
+        colorCode.add(R.color.notgreen);
+
+        Random randomColor = new Random();
+        int number = randomColor.nextInt(colorCode.size());
+        return colorCode.get(number);
 
     }
 
@@ -54,11 +76,13 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder{
         TextView noteTitle,noteContent;
         View view ;
+        CardView mCardView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             noteTitle = itemView.findViewById(R.id.titles);
             noteContent = itemView.findViewById(R.id.content);
+            mCardView = itemView.findViewById(R.id.noteCard);
             view = itemView;
         }
     }
